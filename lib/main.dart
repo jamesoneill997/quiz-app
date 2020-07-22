eebc67f6bca34cc9b62f41b1ca4cd519f068bed6
jamesoneill997 <jamesoneill997@gmail.com>
2020-07-22 19:59:02 +0100

    added stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,57 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 //inheritance, only can extend one class at a time
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex++;
+    });
+    print(questionIndex);
+  }
+
   //makes code clear, overrides build method
   @override
   Widget build(BuildContext ctx) {
+    var questions = [
+      'What is your favourite colour',
+      'What is the best language?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.black,
           title: Text('Sample Quiz'),
         ),
-        body: Text("This is default"),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 4'),
+              onPressed: answerQuestion,
+            ),
+          ],
+        ),
       ),
     );
   }
