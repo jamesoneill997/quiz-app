e1895a458d7fa8e5e816b6fa35517a3cdfeed5ae
jamesoneill997 <jamesoneill997@gmail.com>
2020-07-22 19:16:31 +0100

    hello world basics of dart applied

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+//inheritance, only can extend one class at a time
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext ctx) {
+    return MaterialApp(
+      home: Text('Hello, World!'),
+    );
+  }
+}
