b05c68d03d80533f3c9fa7540b147a0292cef5dc
jamesoneill997 <jamesoneill997@gmail.com>
2020-07-22 19:29:01 +0100

    added appbar and basic styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+//shorthand
+void main() => runApp(MyApp());
 
 //inheritance, only can extend one class at a time
 class MyApp extends StatelessWidget {
+  //makes code clear, overrides build method
+  @override
   Widget build(BuildContext ctx) {
     return MaterialApp(
-      home: Text('Hello, World!'),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Text('Sample Quiz'),
+        ),
+        body: Text("This is default"),
+      ),
     );
   }
 }
